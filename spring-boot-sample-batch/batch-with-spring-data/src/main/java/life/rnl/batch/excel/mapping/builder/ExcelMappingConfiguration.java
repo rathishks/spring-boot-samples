7e524e49b97ccbf6d7efaeec2b4aa652308afaf1
Ryan <ryan@rnl.life>
2017-08-11 13:06:05 -0400

    updates

--- spring-boot-sample-batch/batch-with-spring-data/src/main/java/life/rnl/batch/excel/mapping/builder/ExcelMappingConfiguration.java
+++ spring-boot-sample-batch/batch-with-spring-data/src/main/java/life/rnl/batch/excel/mapping/builder/ExcelMappingConfiguration.java
@@ -1,11 +1,13 @@
 package life.rnl.batch.excel.mapping.builder;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ExcelMappingConfiguration {
 	@Bean
+	@ConditionalOnMissingBean
 	public ExcelMappingBuilder excelMappingBuilder() {
 		return () -> {
 		};
