413023bf263bcf406d707cb876c0a522957eb886
Ryan <ryan@rnl.life>
2017-08-30 19:51:15 -0400

    updates

--- spring-boot-sample-tests/src/test/java/life/rnl/ApplicationTests.java
+++ spring-boot-sample-tests/src/test/java/life/rnl/ApplicationTests.java
@@ -1,15 +1,5 @@
 package life.rnl;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.Calendar;
-
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,16 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import life.rnl.dto.VehicleView;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
-public class ApplicationTests extends BaseTest {
+public class ApplicationTests {
 
 	@Autowired
 	ApplicationContext appContext;
@@ -34,18 +19,4 @@ public class ApplicationTests extends BaseTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@Test
-	public void testRetrieve() throws Exception {
-		this.mockMvc.perform(get("/retrieve/1")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("dateManufactured")));
-		
-		MvcResult result = this.mockMvc.perform(get("/retrieve/1")).andReturn();
-		String json = result.getResponse().getContentAsString();
-		ObjectMapper objectMapper = new ObjectMapper();
-		VehicleView vehicleView = objectMapper.readValue(json, VehicleView.class);
-		Calendar now = Calendar.getInstance();
-		Assert.assertTrue(vehicleView.getDateManufactured().before(now));
-		System.out.println(vehicleView.getDateManufactured());
-		System.out.println(now);
-	}
 }
